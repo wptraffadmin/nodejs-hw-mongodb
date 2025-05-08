7237d2b96a69befb5f0736c52fa3082d43f07342
webgalacticdev <webgalacticdev@proton.me>
2025-05-08 09:35:40 +0300

    done

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,15 +3,16 @@ import { getAllContactsController, createContactController, getContactByIdContro
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { isValidId } from "../middlewares/isValidId.js";
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.put('/contacts/:contactId', ctrlWrapper(replaceContactController));
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(updateContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.put('/contacts/:contactId', isValidId, ctrlWrapper(replaceContactController));
+router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 
 
 export default router;
